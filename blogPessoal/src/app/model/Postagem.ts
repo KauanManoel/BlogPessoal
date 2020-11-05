0ea6241b19b637660d5ff19da1940513b7cfcfef
unknown <kauanmanoel4@gmail.com>
2020-11-05 01:58:02 -0300

    Criação das models de login e cadastro, introdução da segurança nas tais, criação e validação do componente cadastro

---
+++ blogPessoal/src/app/model/Postagem.ts
@@ -0,0 +1,11 @@
+import { Tema } from './Tema';
+
+export class Postagem{
+
+    public id: string;
+    public titulo: string;
+    public texto: string;
+    public data: Date;
+    public tema: Tema
+
+}
